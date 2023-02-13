45ee0eba2604640a3f86c53fbc56c7bcaa95cc00
plumbdog <65794052+JackPlumb@users.noreply.github.com>
2023-02-13 14:38:01 +0000

    Update IvaoTheme.js to feature Poppins font

--- packages/docs/.storybook/IvaoTheme.js
+++ packages/docs/.storybook/IvaoTheme.js
@@ -2,6 +2,11 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'dark',
+
+  // Typography
+  fontBase: '"Poppins", sans-serif',
+  fontCode: 'monospace',
+
   brandTitle: 'IVAO Atmosphere',
   brandUrl: 'https://ivao.aero',
   brandImage: 'https://creators.ivao.aero/[main]/images/logo.png',
