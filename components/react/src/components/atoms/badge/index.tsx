4d0607b3de6e8b383bec3979123fba903e3774d3
Jonas Bellmann <jonas.bellmann@ivao.aero>
2024-01-19 17:42:23 +0100

    style(react): jsx-style attributes

--- components/react/src/components/atoms/badge/index.tsx
+++ components/react/src/components/atoms/badge/index.tsx
@@ -24,7 +24,7 @@ interface RemoveButtonProps {
 
 const Dot: ComponentType = () => {
   return (
-    <svg className="h-1.5 w-1.5" viewBox="0 0 6 6" aria-hidden="true">
+    <svg className={'h-1.5 w-1.5'} viewBox={'0 0 6 6'} aria-hidden={'true'}>
       <circle cx={3} cy={3} r={3} />
     </svg>
   );
@@ -33,7 +33,7 @@ const Dot: ComponentType = () => {
 const RemoveButton = forwardRef<HTMLButtonElement, RemoveButtonProps>(
   ({ isPill, callback }, ref) => (
     <button
-      type="button"
+      type={'button'}
       onClick={callback}
       className={clsx(
         'group relative -mr-0.5 h-3.5 w-3.5 hover:bg-fuselage-500/10 dark:hover:bg-white/5',
@@ -41,9 +41,9 @@ const RemoveButton = forwardRef<HTMLButtonElement, RemoveButtonProps>(
       )}
       ref={ref}
     >
-      <span className="sr-only">Remove</span>
-      <Close className="h-3.5 w-3.5 scale-95 stroke-fuselage-450/50 transition-colors group-hover:stroke-fuselage-450 dark:stroke-white/20 group-hover:dark:stroke-fuselage-200" />
-      <span className="absolute -inset-1" />
+      <span className={'sr-only'}>Remove</span>
+      <Close className={'h-3.5 w-3.5 scale-95 stroke-fuselage-450/50 transition-colors group-hover:stroke-fuselage-450 dark:stroke-white/20 group-hover:dark:stroke-fuselage-200'} />
+      <span className={'absolute -inset-1'} />
     </button>
   ),
 );
