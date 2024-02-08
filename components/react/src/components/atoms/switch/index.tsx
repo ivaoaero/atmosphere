66a05f433e6e509d5e4bf39bad41185195f388ec
Jonas Bellmann <jonas.bellmann@ivao.aero>
2024-02-08 19:40:47 +0100

    refactor(react/switch): move all in atoms

--- components/react/src/components/atoms/switch/index.tsx
+++ components/react/src/components/atoms/switch/index.tsx
@@ -1,2 +1,16 @@
-export { Root } from '@components/atoms/switch/Root';
-export { Thumb } from '@components/atoms/switch/Thumb';
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+
+import { Root } from '@components/atoms/switch/Root';
+import { Thumb } from '@components/atoms/switch/Thumb';
+
+export const Switch = forwardRef<
+  ElementRef<typeof Root>,
+  ComponentPropsWithoutRef<typeof Root>
+>(({ ...props }, ref) => (
+  <Root {...props} ref={ref}>
+    <Thumb />
+  </Root>
+));
+Switch.displayName = Root.displayName;
+
+export { Root as SwitchRoot, Thumb as SwitchThumb };
