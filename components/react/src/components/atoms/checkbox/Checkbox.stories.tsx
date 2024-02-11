2f048cd07bc97f6f3d32faa8f9ae43c559ca148e
David Tchekachev <david.tchekachev@ivao.aero>
2024-02-11 16:07:50 +0100

    feat(react/checkbox): add atom ATMOS-12

---
+++ components/react/src/components/atoms/checkbox/Checkbox.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox } from '@components/atoms/checkbox';
+
+const meta = {
+  title: 'Components/Inputs/Checkbox',
+  component: Checkbox,
+  args: {
+    disabled: false,
+    checked: false,
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+    checked: {
+      control: 'boolean',
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default = {} satisfies Story;
