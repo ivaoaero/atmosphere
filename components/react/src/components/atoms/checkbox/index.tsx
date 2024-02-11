e7f777322a3cc21862895c69431c0cccc7bc6999
Jonas Bellmann <jonas.bellmann@ivao.aero>
2024-02-11 20:47:48 +0100

    refactor(checkbox): react widlcard import

--- components/react/src/components/atoms/checkbox/index.tsx
+++ components/react/src/components/atoms/checkbox/index.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 
 import { CheckboxCheck } from './CheckboxCheck';
 import { CheckboxIndicator } from './CheckboxIndicator';
 import { CheckboxRoot } from './CheckboxRoot';
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxRoot>,
-  React.ComponentPropsWithoutRef<typeof CheckboxRoot>
+const Checkbox = forwardRef<
+  ElementRef<typeof CheckboxRoot>,
+  ComponentPropsWithoutRef<typeof CheckboxRoot>
 >(({ ...props }, ref) => (
   <CheckboxRoot ref={ref} {...props}>
     <CheckboxIndicator checked={props.checked} />
