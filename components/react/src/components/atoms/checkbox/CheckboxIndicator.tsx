6fd5560bc1ab4de18d8e8f899c04a9d6f6bc10de
Jonas Bellmann <jonas.bellmann@ivao.aero>
2024-02-11 20:44:59 +0100

    feat(checkbox): add indeterminate

--- components/react/src/components/atoms/checkbox/CheckboxIndicator.tsx
+++ components/react/src/components/atoms/checkbox/CheckboxIndicator.tsx
@@ -2,18 +2,30 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 
 import { Indicator as RadixIndicator } from '@radix-ui/react-checkbox';
 
+import { CheckboxCheck } from '@components/atoms/checkbox/CheckboxCheck';
+import { CheckboxIndeterminate } from '@components/atoms/checkbox/CheckboxIndeterminate';
+
 import { cn } from '@utils/styles';
 
-const CheckboxIndicator = forwardRef<
-  ElementRef<typeof RadixIndicator>,
-  ComponentPropsWithoutRef<typeof RadixIndicator>
->(({ className, ...props }, ref) => (
-  <RadixIndicator
-    ref={ref}
-    className={cn('flex items-center justify-center text-current', className)}
-    {...props}
-  />
-));
-CheckboxIndicator.displayName = RadixIndicator.displayName;
+interface CheckboxIndicatorProps
+  extends ComponentPropsWithoutRef<typeof RadixIndicator> {
+  checked?: boolean | 'indeterminate';
+}
 
-export { CheckboxIndicator };
+export const CheckboxIndicator = forwardRef<
+  ElementRef<typeof RadixIndicator>,
+  CheckboxIndicatorProps
+>(({ className, children, ...props }, ref) => {
+  const Check =
+    props.checked === 'indeterminate' ? CheckboxIndeterminate : CheckboxCheck;
+  return (
+    <RadixIndicator
+      ref={ref}
+      className={cn('flex items-center justify-center text-current', className)}
+      {...props}
+    >
+      {children ?? <Check />}
+    </RadixIndicator>
+  );
+});
+CheckboxIndicator.displayName = RadixIndicator.displayName;
