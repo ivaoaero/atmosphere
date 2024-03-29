4e6c83185461a6ade8ac12a80f419d5dacbe5725
David Tchekachev <david.tchekachev@ivao.aero>
2024-03-30 00:19:34 +0100

    feat(react/dropdown-menu): add `DropdownMenu` atoms ATMOS-21

---
+++ components/react/src/components/atoms/dropdown-menu/DropdownMenuItem.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+
+import { Item } from '@radix-ui/react-dropdown-menu';
+
+import { cn } from '@utils/styles';
+
+export const DropdownMenuItem = forwardRef<
+  ElementRef<typeof Item>,
+  ComponentPropsWithoutRef<typeof Item> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <Item
+    ref={ref}
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      inset && 'pl-8',
+      className,
+    )}
+    {...props}
+  />
+));
+DropdownMenuItem.displayName = Item.displayName;
