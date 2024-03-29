c4740a9899263b4cb5128df9ba200b6da25034d4
David Tchekachev <david.tchekachev@ivao.aero>
2024-03-29 14:46:21 +0100

    feat(react/card): add `Card` atoms ATMOS-10

---
+++ components/react/src/components/atoms/card/CardFooter.tsx
@@ -0,0 +1,15 @@
+import { forwardRef, HTMLAttributes } from 'react';
+
+import { cn } from '@utils/styles';
+
+export const CardFooter = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn('flex items-center p-6 pt-0', className)}
+    {...props}
+  />
+));
+CardFooter.displayName = 'CardFooter';
