13c2c248b2fb0bfd97517fe107a40de5da16156e
David Tchekachev <david.tchekachev@ivao.aero>
2024-03-29 21:56:04 +0100

    feat(react/alert): add `Alert` atoms

---
+++ components/react/src/components/atoms/alert/index.tsx
@@ -0,0 +1,3 @@
+export { AlertRoot } from '@components/atoms/alert/AlertRoot';
+export { AlertTitle } from '@components/atoms/alert/AlertTitle';
+export { AlertDescription } from '@components/atoms/alert/AlertDescription';
