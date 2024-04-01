33a60a530df9d42e18d0f7c5247657df7983b960
David Tchekachev <david.tchekachev@ivao.aero>
2024-04-01 19:17:33 +0200

    feat(react/sidebar): add `Sidebar` atoms

---
+++ components/react/src/components/atoms/sidebar/SidebarItem.tsx
@@ -0,0 +1,73 @@
+import {
+  AnchorHTMLAttributes,
+  ComponentType,
+  HTMLAttributes,
+  PropsWithRef,
+} from 'react';
+
+import clsx from 'clsx';
+
+import { useSidebar } from '@hooks/useSidebar';
+
+type AnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+
+export type SidebarAsLinkProps = PropsWithRef<AnchorProps> &
+  Required<Pick<AnchorProps, 'href'>>;
+
+interface SidebarItemProps extends HTMLAttributes<HTMLAnchorElement> {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  href: string;
+  asLink: ComponentType<SidebarAsLinkProps>;
+  isActive?: boolean;
+}
+
+export const SidebarItem = ({
+  title,
+  description,
+  icon,
+  href,
+  asLink,
+  isActive,
+  ...props
+}: SidebarItemProps) => {
+  const { isSidebarOpen } = useSidebar();
+
+  const LinkComponent = asLink ?? 'a';
+
+  // Default match active link
+  if (isActive === undefined) isActive = window.location.pathname === href;
+
+  return (
+    <LinkComponent
+      href={href}
+      className="flex dark:text-fuselage-400 text-fuselage-300 group"
+      {...props}
+    >
+      <div
+        className={clsx(
+          'rounded-md p-2 transition-all',
+          isActive
+            ? 'bg-semantic-green-500 text-white dark:bg-semantic-green-500/10 dark:text-semantic-green-500'
+            : 'bg-fuselage-50/40 text-fuselage-300 dark:text-fuselage-400 dark:bg-fuselage-700 group-hover:text-fuselage-400 group-hover:bg-fuselage-50/75 dark:group-hover:bg-fuselage-600 dark:group-hover:text-fuselage-100',
+        )}
+      >
+        {icon}
+      </div>
+
+      <div
+        className={clsx(
+          'transition-all flex flex-col items-start shrink-0 whitespace-nowrap',
+          isSidebarOpen ? 'w-48 opacity-100 ml-4' : 'w-0 opacity-0 invisible',
+        )}
+      >
+        <span className="font-semibold font-head dark:text-fuselage-50 text-fuselage-500 leading-tight">
+          {title}
+        </span>
+        <span className="text-sm">{description}</span>
+      </div>
+    </LinkComponent>
+  );
+};
+SidebarItem.displayName = 'SidebarItem';
---
+++ components/react/src/components/atoms/sidebar/index.tsx
@@ -0,0 +1,3 @@
+export { SidebarContainer } from '@components/atoms/sidebar/SidebarContainer';
+export { SidebarCollapseButton } from '@components/atoms/sidebar/SidebarCollapseButton';
+export { SidebarItem } from '@components/atoms/sidebar/SidebarItem';
