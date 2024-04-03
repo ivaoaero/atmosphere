4bdae36e12e33164895649f697bf9a32e88cacf0
David Tchekachev <david.tchekachev@ivao.aero>
2024-04-03 11:42:50 +0200

    chore(react/sidebar): export props typings

--- components/react/src/components/atoms/sidebar/index.tsx
+++ components/react/src/components/atoms/sidebar/index.tsx
@@ -1,3 +1,7 @@
 export { SidebarContainer } from '@components/atoms/sidebar/SidebarContainer';
 export { SidebarCollapseButton } from '@components/atoms/sidebar/SidebarCollapseButton';
 export { SidebarItem } from '@components/atoms/sidebar/SidebarItem';
+export type {
+  SidebarItemProps,
+  SidebarAsLinkProps,
+} from '@components/atoms/sidebar/SidebarItem';
