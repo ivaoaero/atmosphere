27ee60fda5e3ab0708225b997e7a64d2476de331
David Tchekachev <david.tchekachev@ivao.aero>
2024-04-09 10:53:34 +0200

    fix(react/icon-button): use `forwardRef`

--- components/react/src/components/atoms/icon-button/index.tsx
+++ components/react/src/components/atoms/icon-button/index.tsx
@@ -1,29 +1,27 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, ElementRef, forwardRef, ReactNode } from 'react';
 
 import { Button } from '@components/atoms/button';
 
 import { cn } from '@utils/styles';
 
 interface IconButtonProps extends ComponentProps<typeof Button> {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-export const IconButton = ({
-  children,
-  className,
-  ...props
-}: IconButtonProps) => {
-  return (
-    <Button
-      type="button"
-      className={cn(
-        'dark:text-fuselage-300 text-ocean-100 hover:text-ocean-50 hover:bg-ocean-600 flex gap-1 items-center dark:hover:bg-fuselage-700 dark:hover:text-fuselage-100 rounded-md p-1.5 transition-all',
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </Button>
-  );
-};
+export const IconButton = forwardRef<
+  ElementRef<typeof Button>,
+  IconButtonProps
+>(({ children, className, ...props }, ref) => (
+  <Button
+    ref={ref}
+    type="button"
+    className={cn(
+      'dark:text-fuselage-300 text-ocean-100 hover:text-ocean-50 hover:bg-ocean-600 flex gap-1 items-center dark:hover:bg-fuselage-700 dark:hover:text-fuselage-100 rounded-md p-1.5 transition-all',
+      className,
+    )}
+    {...props}
+  >
+    {children}
+  </Button>
+));
 IconButton.displayName = 'IconButton';
