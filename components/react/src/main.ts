e096e64500de9c04b9bd2f3013bdd0754dfe8784
David Tchekachev <david.tchekachev@ivao.aero>
2024-04-26 17:39:49 +0200

    chore(react/radio-group): export molecule props type

--- components/react/src/main.ts
+++ components/react/src/main.ts
@@ -89,6 +89,10 @@ export type {
 export { NavigationMenu } from '@components/molecules/navigation-menu';
 export { RadioGroupRoot, RadioGroupItem } from '@components/atoms/radio-group';
 export { RadioGroup } from '@components/molecules/radio-group';
+export type {
+  RadioGroupProps,
+  RadioGroupItemProps,
+} from '@components/molecules/radio-group';
 export {
   SelectRoot,
   SelectPortal,
