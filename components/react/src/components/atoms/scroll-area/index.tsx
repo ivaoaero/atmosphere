0d1280961d2571ad3b4aaacbbe537de4d9da4956
David Tchekachev <david.tchekachev@ivao.aero>
2024-05-22 16:49:22 +0200

    feat(react/scroll-area): add `ScrollArea` atoms and molecule ATMOS-34

---
+++ components/react/src/components/atoms/scroll-area/index.tsx
@@ -0,0 +1,9 @@
+export {
+  Corner as ScrollAreaCorner,
+  ScrollAreaScrollbar,
+  ScrollAreaThumb,
+} from '@radix-ui/react-scroll-area';
+
+export { ScrollAreaViewport } from '@components/atoms/scroll-area/ScrollAreaViewport';
+export { ScrollAreaRoot } from '@components/atoms/scroll-area/ScrollAreaRoot';
+export { ScrollBar } from '@components/atoms/scroll-area/ScrollBar';
