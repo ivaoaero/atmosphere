46562197c1a21d7a2bc64fd033605e90d318bfb8
alfa <alfamodz26@gmail.com>
2024-06-04 19:23:53 +0200

    chore: added changelog entry and exports

--- components/react/src/main.ts
+++ components/react/src/main.ts
@@ -115,6 +115,13 @@ export {
 export type { PaginationLinkProps } from '@components/atoms/pagination/PaginationLink';
 export { Pagination } from '@components/molecules/pagination';
 export type { PaginationProps } from '@components/molecules/pagination';
+export {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverPortal,
+  PopoverTrigger,
+} from '@components/atoms/popover';
 export { RadioGroupRoot, RadioGroupItem } from '@components/atoms/radio-group';
 export { RadioGroup } from '@components/molecules/radio-group';
 export type {
