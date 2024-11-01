0567e6c137e27d0e9121174503d3ff411a8a03fb
David Tchekachev <david.tchekachev@ivao.aero>
2024-11-01 23:15:54 +0100

    feat(react/accordion): add `Accordion` atoms and molecules

---
+++ components/react/src/components/atoms/accordion/AccordionItem.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+
+import { Item } from '@radix-ui/react-accordion';
+
+import { cn } from '@utils/styles';
+
+export const AccordionItem = forwardRef<
+  ElementRef<typeof Item>,
+  ComponentPropsWithoutRef<typeof Item>
+>(({ className, ...props }, ref) => (
+  <Item ref={ref} className={cn('border-b', className)} {...props} />
+));
+AccordionItem.displayName = Item.displayName;
