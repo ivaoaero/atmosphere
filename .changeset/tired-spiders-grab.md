ccc33cb5ce5305f336ed292c247d5fa172b7b477
Felipe Lobo <wolfmaster8@users.noreply.github.com>
2025-04-08 10:26:27 -0500

    feat: update button design, update readme & fix styles (#76)
    * chore: update readme & add build and storybbok commands
    
    * feat: update button variants
    
    * feat: add loading to button
    
    * feat: update fuselage-100 to a better tone of gray. Former one was blueish
    
    * chore: fix font Nunito Sans
    
    * chore: update font sizes for button
    
    * feat: update dark button colors to white
    
    * chore: fix dark primary disabled button
    
    * chore: fix secondary bg contrast
    
    * chore: add changeset
    
    * chore: update pkg build name to prebuild

---
+++ .changeset/tired-spiders-grab.md
@@ -0,0 +1,6 @@
+---
+"@ivao/atmosphere-react": minor
+"@ivao/atmosphere-brand": minor
+---
+
+Updated the design of the button. Removed green on dark mode. Fixed font on the @ivao/atmosphere-brand package
--- components/react/src/components/atoms/button/index.tsx
+++ components/react/src/components/atoms/button/index.tsx
@@ -1,6 +1,8 @@
 import { ButtonHTMLAttributes, forwardRef } from 'react';
 
+import { Slot } from '@radix-ui/react-slot';
 import { VariantProps } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
 
 import { buttonVariants } from '@components/atoms/button/buttonVariants';
 
@@ -8,18 +10,41 @@ import { cn } from '@utils/styles';
 
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  isLoading?: boolean;
+}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
-    // TODO: if is loading, insert a spinner
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading = false,
+      children,
+      disabled,
+      ...props
+    },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : 'button';
+    const isDisabled = disabled || isLoading;
+
     return (
-      <button
-        className={cn(buttonVariants({ variant, size }), className)}
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={isDisabled}
+        aria-disabled={isDisabled}
         ref={ref}
         {...props}
-      />
+      >
+        {children}
+        {isLoading ? <Loader2 className="size-4 animate-spin" /> : null}
+      </Comp>
     );
   },
 );
+
 Button.displayName = 'Button';
