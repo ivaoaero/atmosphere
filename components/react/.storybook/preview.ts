d9b81fa7cc7cc64183c195026d4127e42eee02ce
Jonas <jonas.bellmann@ivao.aero>
2025-10-14 00:00:11 +0200

    feat(colors)!: add body color (#114)
    BREAKING CHANGE: `--background` variable is used for various backgrounds. Adding a new variable `--body` to improve contrast and minimal efforts

--- components/react/.storybook/preview.ts
+++ components/react/.storybook/preview.ts
@@ -13,7 +13,7 @@ const preview: Preview = {
     backgrounds: {
       default: 'theme',
       options: {
-        theme: { name: 'theme', value: `rgb(var(--background))` },
+        theme: { name: 'theme', value: `rgb(var(--body))` },
       },
     },
   },
