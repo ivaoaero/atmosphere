c1c59d0971855fa2fab6c79e260126a3cc290232
Jonas Bellmann <jonas.bellmann@ivao.aero>
2026-07-30 14:30:13 +0200

    feat(data-table): use `meta.name` in view options
    closes #130
    
    Co-authored-by: Antonio Bonet <tonybonet31@yahoo.es>

--- components/react/src/tanstack-table.d.ts
+++ components/react/src/tanstack-table.d.ts
@@ -4,5 +4,6 @@ declare module '@tanstack/react-table' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     className?: string;
+    name?: string;
   }
 }
