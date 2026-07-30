6d56c68ad9edfd7a26c9492242d775a4497e6c57
Jessie Sedman <connorsedman.vatsim@gmail.com>
2026-07-30 13:42:16 +0100

    Fix default variant background styling for dark mode (#138)
    Co-authored-by: Jonas Bellmann <jonas.bellmann@ivao.aero>

--- components/react/src/components/atoms/alert/alertVariants.ts
+++ components/react/src/components/atoms/alert/alertVariants.ts
@@ -8,9 +8,9 @@ export const alertVariants = cva(
         default:
           'border-fuselage-300 bg-background text-foreground dark:border-fuselage-600 dark:[&>h5]:text-fuselage-50',
         success:
-          'text-semantic-green-700 dark:text-semantic-green-300 dark:[&>h5]:text-semantic-green-500 [&>svg]:text-semantic-green-500 border-green-600 dark:border-green-500',
+          'text-semantic-green-700 dark:text-semantic-green-300 dark:[&>h5]:text-semantic-green-500 [&>svg]:text-semantic-green-500 border-green-600 bg-green-50 dark:border-green-500 dark:bg-green-950',
         destructive:
-          'border-destructive text-semantic-red-600 dark:border-destructive dark:text-destructive-foreground dark:[&>h5]:text-destructive [&>svg]:text-destructive',
+          'border-destructive text-semantic-red-600 dark:border-destructive dark:text-destructive-foreground dark:[&>h5]:text-destructive [&>svg]:text-destructive bg-red-50 dark:bg-red-950',
       },
     },
     defaultVariants: {
