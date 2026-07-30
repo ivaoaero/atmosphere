76c9764d9d4869d56a86cc3b8f8a3c5afa200aa1
Jonas Bellmann <jonas.bellmann@ivao.aero>
2026-07-30 13:46:35 +0200

    refactor(navbar): simplify props

--- components/react/src/components/molecules/navbar/index.tsx
+++ components/react/src/components/molecules/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentType, PropsWithChildren, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ComponentType, ReactNode } from 'react';
 
 import { NavbarContainer } from '@components/atoms/navbar';
 
@@ -6,20 +6,24 @@ import { cn } from '@utils/styles';
 
 import { IVAOLogo } from 'src/main';
 
-interface NavbarProps {
+interface NavbarProps extends Omit<
+  ComponentPropsWithoutRef<typeof NavbarContainer>,
+  'title'
+> {
   title: ReactNode;
   logoVariant?: 'icon-only';
   diagonalDivider?: boolean;
 }
 
-export const Navbar: ComponentType<PropsWithChildren<NavbarProps>> = ({
+export const Navbar: ComponentType<NavbarProps> = ({
   title,
   children,
   logoVariant,
   diagonalDivider = false,
+  ...props
 }) => {
   return (
-    <NavbarContainer>
+    <NavbarContainer {...props}>
       <div className={'flex items-center gap-3'}>
         {logoVariant === 'icon-only' ? (
           <IVAOLogo color={'white'} onlyIcon />
