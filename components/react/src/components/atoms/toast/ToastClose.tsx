488029726c4545575c106a79340e5101d8a17ce8
Jonas Bellmann <jonas.bellmann@ivao.aero>
2026-08-12 14:34:17 +0200

    fix(toast): remove group class specifics from close button

--- components/react/src/components/atoms/toast/ToastClose.tsx
+++ components/react/src/components/atoms/toast/ToastClose.tsx
@@ -19,7 +19,7 @@ export const ToastClose = forwardRef<
       {...props}
       aria-label={ariaLabel}
       className={cn(
-        'text-foreground/50 hover:text-foreground group-[.destructive]:text-semantic-red-300 group-[.destructive]:hover:text-semantic-red-50 absolute top-1 right-1 rounded-md p-1 opacity-0 transition-opacity group-hover:opacity-100 focus:opacity-100 focus:ring-1 focus:outline-hidden group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600',
+        'text-foreground/50 hover:text-foreground absolute top-1 right-1 rounded-md p-1 opacity-0 transition-opacity group-hover:opacity-100 focus:opacity-100 focus:ring-1 focus:outline-hidden',
         className,
       )}
       ref={ref}
